770302145d02572adefcd5d708bc3809a83848a5
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2023-12-11 17:27:41 +0100

    Add files via upload

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -1,7 +1,13 @@
 import { HelloWorld } from "./helloworld";
+import { Message } from "./Message";
 
 export function App() {
-    return <div>
+    return (
+    <div>
+        <HelloWorld /> {/*nothing wrong happen, the hello func will be printed on screen already*/}
         <HelloWorld />
+        <p>What a beautiful day</p>
+        <Message /> {/*App function become useless because we can't use more than an import inside the function*/}
     </div>
-}
+)}
+
