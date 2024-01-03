166e2d15eab8da303de12dbd8a870ebfefb9b885
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-01-03 18:14:00 +0100

    Add files via upload

--- latest/src/MouseClicker.jsx
+++ latest/src/MouseClicker.jsx
@@ -1,11 +1,11 @@
 export function MouseClicker() {
     function handleButtonClick(event) {
-        console.log(event.currentTarget.name)
+        console.log(event.currentTarget.src)
     }
     return (
         <div>
     <button name="btn1" onClick={handleButtonClick}>
-        Click me
+     <img src="img" width={24} height={24} onClick={handleButtonClick}></img>   Click me
     </button>
     </div>
     )
