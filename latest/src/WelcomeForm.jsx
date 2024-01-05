c5b87c44ff60d82229ef7f78212e8864018ff968
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-01-05 18:44:26 +0100

    Add files via upload

--- latest/src/WelcomeForm.jsx
+++ latest/src/WelcomeForm.jsx
@@ -10,11 +10,13 @@ export function WelcomeForm() {
     function handleInputChange(event) {
         const name = event.target.name
         const value = event.target.value
+        const checked = event.target.checked
+        const type = event.target.type
 
         setData((data) => {
             return {
                 ...data,
-                [name]:value
+                [name]: type === "checkbox" ? checked : value,
             }
         })
     }
@@ -22,7 +24,8 @@ export function WelcomeForm() {
     function handleResetForm(){
         setData({
             username:"",
-            password:""
+            password:"",
+            session: false
         })
     }
     return( 
@@ -30,7 +33,7 @@ export function WelcomeForm() {
         <h1>My form</h1>
     <input name= "username" value={data.username} onChange={handleInputChange} />
     <input name= "password" type="password" value={data.password} onChange={handleInputChange} />
-    <input name="session" type="checkbox" value={data.session} onChange={handleInputChange} />
+    <input name="session" type="checkbox" checked={data.session} onChange={handleInputChange} />
     <button onClick={handleResetForm}>Reset</button>
     <button disabled= {!data.username || !data.password} onClick={handleResetForm}>Login</button>
     </div>
