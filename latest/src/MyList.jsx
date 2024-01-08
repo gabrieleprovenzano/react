37b0c418f564a1d51c1963872c8e20f776d2e55d
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-01-08 17:59:14 +0100

    Add files via upload

--- latest/src/MyList.jsx
+++ latest/src/MyList.jsx
@@ -4,12 +4,18 @@ export function MyListItem({item}) {
     const [counter , setCounter] = useState(0)
 
     function handleIncrementCounter() {
-        setCounter((el)=> el+1)
+        setCounter((el)=> el + 1)
+    }
+
+    function Reset() {
+        setCounter("")
     }
     return(
         <li key={item.id}>
-            <h4>{item.name}</h4>
-            <button onClick={handleIncrementCounter} >{counter}</button>
+            <h4>{item.name}{counter}</h4>
+            <button onClick={handleIncrementCounter}>Increment</button>
+            <button onClick={Reset}>Reset</button>
+            <input type="text" value={counter}/>
         </li>
     )
 }
