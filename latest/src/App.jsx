aafbffcefbfb973a89b8c33ea48ffdb5d8808f92
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-01-17 18:25:38 +0100

    Add files via upload

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -1,6 +1,8 @@
 
+import { GitHubUser } from "./GitHubUser";
 import { HelloWorld } from "./HelloWorld";
 import { HookCounter } from "./HookCounter";
+import { HookForm } from "./HookForm";
 import Welcome  from "./Welcome";
 import { YourAge } from "./YourAgeIs";
 import { useState } from "react";
@@ -15,6 +17,8 @@ export function App() {
         <hr />
         <Welcome name="Jimmy"/>
         <YourAge age={19}/>
-        <HookCounter />
+        <HookCounter initialValue={99}/>
+        <GitHubUser username="gabrieleprovenzano"/>
+        <HookForm />
         </div>
 )}
