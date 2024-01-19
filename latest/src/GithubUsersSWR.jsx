9e518e35b0adb2d15eff405e72f3bcdb183b2b27
Gabriele Provenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2024-01-19 12:21:50 +0100

    Add files via upload

--- latest/src/GithubUsersSWR.jsx
+++ latest/src/GithubUsersSWR.jsx
@@ -3,10 +3,11 @@ import { useGitHubUsersSWR } from "./useGitHubUsersSWR";
 
 
 export function GithubUsersSWR() {
-    const {users , error , isLoading} = useGitHubUsersSWR()
+    const {users , error , isLoading , onRefresh} = useGitHubUsersSWR()
 
     return(
         <div>
+            <button onClick={onRefresh}>Refresh</button>
             {isLoading && <h3>Loading...</h3>}
             {error && <h3>An error as occuped;</h3>}
             {users && (
--- latest/src/useGitHubUsersSWR.jsx
+++ latest/src/useGitHubUsersSWR.jsx
@@ -3,11 +3,16 @@ import useSWR from "swr"
 const fetcher = url => fetch(url) .then((r)=>r.json())
 
 export function useGitHubUsersSWR() {
-    const { data , error } = useSWR("https://api.github.com/users" , fetcher)
+    const { data , error , mutate} = useSWR("https://api.github.com/users" , fetcher)
+
+    function onHandleRefresh() {
+        mutate()
+    }
 
     return {
         users: data,
         error ,
-        isLoading: !data && !error
+        isLoading: !data && !error,
+        onRefresh: onHandleRefresh()
     }
 }
